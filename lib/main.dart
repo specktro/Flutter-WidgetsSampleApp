d664cf1c1422a25a1095833601996d0a4a8391ca
@specktro <specktro@nonull.mx>
2025-06-07 09:20:55 -0600

    feat: Implement basic app theming
    This commit introduces a basic theming system for the application.
    
    - A predefined list of `Color`s (`colorList`) is now available in `lib/config/app_theme.dart`.
    - The `AppTheme` class is created to manage the application's theme. It takes a `selectedColor` index to choose a color from `colorList` as the `colorSchemeSeed` for the `ThemeData`.
    - The `MaterialApp` in `main.dart` now uses `AppTheme().getTheme()` to apply the selected theme. The `selectedColor` is currently hardcoded to 8.

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:widgets_app/config/app_theme.dart';
 
 void main() {
   runApp(const MainApp());
@@ -9,8 +10,9 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       debugShowCheckedModeBanner: false,
+      theme: AppTheme(selectedColor: 8).getTheme(),
       home: Scaffold(
         body: Center(
           child: Text('Hello World!')
