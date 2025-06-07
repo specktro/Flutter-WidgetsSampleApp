f328611bdc072ec4be60ef1bc151af930416304c
@specktro <specktro@nonull.mx>
2025-06-06 18:32:32 -0600

    chore: Remove debug mode banner from app

--- lib/main.dart
+++ lib/main.dart
@@ -10,11 +10,12 @@ class MainApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: Scaffold(
         body: Center(
-          child: Text('Hello World!'),
-        ),
-      ),
+          child: Text('Hello World!')
+        )
+      )
     );
   }
 }
