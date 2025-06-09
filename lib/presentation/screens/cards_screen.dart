ff2ea9e7be69e7d10a66aaacd69b368995017809
@specktro <specktro@nonull.mx>
2025-06-08 19:19:00 -0600

    feat: Introduce new card type and enhance CardsScreen
    This commit introduces a new card type, `_CardType2`, and updates the `CardsScreen` to display both `_CardType1` and the new `_CardType2` cards.
    
    - **New `_CardType2` Widget:**
        - Displays a `label` and has a specified `elevation`.
        - Features a `RoundedRectangleBorder` with a circular radius and an outline color derived from the current theme's `colorScheme.outline`.
        - Includes an `IconButton` (more_vert_outlined) aligned to the top right.
        - Displays the `label` text appended with " - outline" aligned to the bottom left.
    
    - **`CardsScreen` Enhancements:**
        - The `_CardsView` now renders a list of `_CardType1` widgets followed by a list of `_CardType2` widgets, both generated from the `cards` data.
        - A `SizedBox` with a height of 30 is added at the end of the `Column` for spacing.

--- lib/presentation/screens/cards_screen.dart
+++ lib/presentation/screens/cards_screen.dart
@@ -49,7 +49,11 @@ class _CardsView extends StatelessWidget {
     return SingleChildScrollView(
       child: Column(
         children: [
-          ...cards.map((card) => _CardType1(elevation: card['elevation'], label: card['label']))
+          ...cards.map((card) =>
+              _CardType1(elevation: card['elevation'], label: card['label'])),
+          ...cards.map((card) =>
+              _CardType2(elevation: card['elevation'], label: card['label'])),
+          const SizedBox(height: 30)
         ],
       ),
     );
@@ -65,25 +69,65 @@ class _CardType1 extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Card(
-      elevation: elevation,
-      child: Padding(
-        padding: const EdgeInsets.fromLTRB(10, 5, 10, 10),
-        child: Column(
-          children: [
-            Align(
-              alignment: Alignment.topRight,
-              child: IconButton(
-                icon: Icon(Icons.more_vert_outlined),
-                onPressed: () {}
-              )
-            ),
-            Align(
-              alignment: Alignment.bottomLeft,
-              child: Text(label)
+        elevation: elevation,
+        child: Padding(
+            padding: const EdgeInsets.fromLTRB(10, 5, 10, 10),
+            child: Column(
+              children: [
+                Align(
+                    alignment: Alignment.topRight,
+                    child: IconButton(
+                        icon: Icon(Icons.more_vert_outlined),
+                        onPressed: () {}
+                    )
+                ),
+                Align(
+                    alignment: Alignment.bottomLeft,
+                    child: Text(label)
+                )
+              ],
             )
-          ],
         )
-      )
+    );
+  }
+}
+
+class _CardType2 extends StatelessWidget {
+  final String label;
+  final double elevation;
+
+  const _CardType2({required this.label, required this.elevation});
+
+  @override
+  Widget build(BuildContext context) {
+    final colors = Theme.of(context).colorScheme;
+
+    return Card(
+        shape: RoundedRectangleBorder(
+          borderRadius: const BorderRadius.all(Radius.circular(12)),
+          side: BorderSide(
+            color: colors.outline
+          )
+        ),
+        elevation: elevation,
+        child: Padding(
+            padding: const EdgeInsets.fromLTRB(10, 5, 10, 10),
+            child: Column(
+              children: [
+                Align(
+                    alignment: Alignment.topRight,
+                    child: IconButton(
+                        icon: Icon(Icons.more_vert_outlined),
+                        onPressed: () {}
+                    )
+                ),
+                Align(
+                    alignment: Alignment.bottomLeft,
+                    child: Text('$label - outline')
+                )
+              ],
+            )
+        )
     );
   }
 }
