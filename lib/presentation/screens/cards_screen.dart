3fdcb5747431868c74d56024b07e34810098ccb3
@specktro <specktro@nonull.mx>
2025-06-08 18:00:01 -0600

    feat: Implement Cards Screen with various elevations
    This commit introduces the `CardsScreen` which displays a list of Material `Card` widgets with different elevation levels.
    
    - A `cards` list is defined, containing maps with `elevation` and `label` for each card.
    - The `CardsScreen` now has a `_CardsView` as its body.
    - `_CardsView` is a `StatelessWidget` that uses `SingleChildScrollView` and `Column` to display a list of `_CardType1` widgets.
    - `_CardType1` is a `StatelessWidget` that takes `label` and `elevation` as parameters. It renders a `Card` with the specified elevation, containing an `IconButton` (more_vert_outlined) aligned to the top right and a `Text` widget (displaying the label) aligned to the bottom left.

--- lib/presentation/screens/cards_screen.dart
+++ lib/presentation/screens/cards_screen.dart
@@ -1,5 +1,32 @@
 import 'package:flutter/material.dart';
 
+const List<Map<String, dynamic>> cards = [
+  {
+    'elevation': 0.0,
+    'label': 'Elevation 0'
+  },
+  {
+    'elevation': 1.0,
+    'label': 'Elevation 1'
+  },
+  {
+    'elevation': 2.0,
+    'label': 'Elevation 2'
+  },
+  {
+    'elevation': 3.0,
+    'label': 'Elevation 3'
+  },
+  {
+    'elevation': 4.0,
+    'label': 'Elevation 4'
+  },
+  {
+    'elevation': 5.0,
+    'label': 'Elevation 5'
+  }
+];
+
 class CardsScreen extends StatelessWidget {
   const CardsScreen({super.key});
 
@@ -9,7 +36,54 @@ class CardsScreen extends StatelessWidget {
       appBar: AppBar(
           title: const Text('Cards Screen')
       ),
-      body: Placeholder(),
+      body: _CardsView(),
+    );
+  }
+}
+
+class _CardsView extends StatelessWidget {
+  const _CardsView();
+
+  @override
+  Widget build(BuildContext context) {
+    return SingleChildScrollView(
+      child: Column(
+        children: [
+          ...cards.map((card) => _CardType1(elevation: card['elevation'], label: card['label']))
+        ],
+      ),
+    );
+  }
+}
+
+class _CardType1 extends StatelessWidget {
+  final String label;
+  final double elevation;
+
+  const _CardType1({required this.label, required this.elevation});
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      elevation: elevation,
+      child: Padding(
+        padding: const EdgeInsets.fromLTRB(10, 5, 10, 10),
+        child: Column(
+          children: [
+            Align(
+              alignment: Alignment.topRight,
+              child: IconButton(
+                icon: Icon(Icons.more_vert_outlined),
+                onPressed: () {}
+              )
+            ),
+            Align(
+              alignment: Alignment.bottomLeft,
+              child: Text(label)
+            )
+          ],
+        )
+      )
     );
   }
 }
