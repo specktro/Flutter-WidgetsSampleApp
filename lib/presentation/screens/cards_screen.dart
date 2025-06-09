b0df53f61d690da7ebae6341604743f3539a860c
@specktro <specktro@nonull.mx>
2025-06-08 19:48:25 -0600

    feat: Add image card with overlayed button
    This commit introduces a new card type, `_CardType4`, to `CardsScreen`.
    
    - `_CardType4` displays an image fetched from `picsum.photos`.
    - It includes an `IconButton` with a "more_vert_outlined" icon, positioned at the top-right corner of the card.
    - The button is placed within a `Container` with a white background and rounded bottom-left corner to ensure visibility against the image.
    - `clipBehavior: Clip.hardEdge` is used on the `Card` to ensure the image respects the card's rounded corners.

--- lib/presentation/screens/cards_screen.dart
+++ lib/presentation/screens/cards_screen.dart
@@ -52,6 +52,7 @@ class _CardsView extends StatelessWidget {
           ...cards.map((card) => _CardType1(elevation: card['elevation'], label: card['label'])),
           ...cards.map((card) => _CardType2(elevation: card['elevation'], label: card['label'])),
           ...cards.map((card) => _CardType3(elevation: card['elevation'], label: card['label'])),
+          ...cards.map((card) => _CardType4(elevation: card['elevation'], label: card['label'])),
           const SizedBox(height: 30)
         ],
       ),
@@ -161,6 +162,43 @@ class _CardType3 extends StatelessWidget {
                 )
               ],
             )
+        )
+    );
+  }
+}
+
+class _CardType4 extends StatelessWidget {
+  final String label;
+  final double elevation;
+
+  const _CardType4({required this.label, required this.elevation});
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      clipBehavior: Clip.hardEdge,
+        elevation: elevation,
+        child: Stack(
+          children: [
+            Image.network(
+              'https://picsum.photos/id/${elevation.toInt()}/600/350',
+              height: 350,
+              fit: BoxFit.cover
+            ),
+            Align(
+                alignment: Alignment.topRight,
+                child: Container(
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.only(bottomLeft: Radius.circular(20))
+                  ),
+                  child: IconButton(
+                      icon: Icon(Icons.more_vert_outlined),
+                      onPressed: () {}
+                  ),
+                )
+            ),
+          ],
         )
     );
   }
