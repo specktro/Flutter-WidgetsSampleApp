0cc28fc9c42a72f36482e90aedc9cb28cc043af8
specktro <miguelangel.gomez@globant.com>
2025-06-09 10:56:14 -0600

    feat: Implement controlled progress indicators
    This commit updates the `_ProgressView` widget to display and animate both `CircularProgressIndicator` and `LinearProgressIndicator`.
    
    - A `StreamBuilder` is used to generate progress values from 0 to 100 over time.
    - The `CircularProgressIndicator` now:
        - Displays the current `progressValue`.
        - Has a `strokeWidth` of 2.
        - Sets `Colors.black12` as its `backgroundColor`.
    - The `LinearProgressIndicator` also displays the current `progressValue`.
    - Both indicators are arranged in a `Row` with spacing.

--- lib/presentation/screens/progress_screen.dart
+++ lib/presentation/screens/progress_screen.dart
@@ -41,6 +41,31 @@ class _ControlledProgressIndicator extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Placeholder();
+    return StreamBuilder(
+      stream: Stream.periodic(
+        const Duration(milliseconds: 300), 
+        (value) => value * 2 / 100
+      ).takeWhile((value) => value < 100),
+      builder: (context, asyncSnapshot) {
+        final progressValue = asyncSnapshot.data ?? 0;
+        return Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 20),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              CircularProgressIndicator(
+                value: progressValue,
+                strokeWidth: 2,
+                backgroundColor: Colors.black12
+              ),
+              const SizedBox(width: 20),
+              Expanded(
+                child: LinearProgressIndicator(value: progressValue)
+              )
+            ],
+          ),
+        );
+      }
+    );
   }
 }
