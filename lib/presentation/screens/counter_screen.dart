158f3adbe0d65ee8e4ab81039c3f726cd07e253e
specktro <miguelangel.gomez@globant.com>
2025-06-11 16:43:05 -0600

    feat: Integrate Riverpod for state management
    This commit introduces Riverpod for state management in the application.
    
    - Added `flutter_riverpod` dependency.
    - Wrapped `MainApp` with `ProviderScope` in `main.dart`.
    - Created `counterProvider` to manage the counter state, initialized to 5.
    - Created `isDarkModeProvider` to manage the dark mode state, initialized to false.
    - Updated `CounterScreen` to be a `ConsumerWidget`.
        - It now displays the counter value from `counterProvider`.
        - The FloatingActionButton increments the counter value.
        - An `IconButton` is added to the `AppBar` to toggle the dark mode state using `isDarkModeProvider`.

--- lib/presentation/screens/counter_screen.dart
+++ lib/presentation/screens/counter_screen.dart
@@ -1,19 +1,35 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:widgets_app/presentation/providers/counter_provider.dart';
+import 'package:widgets_app/presentation/providers/theme_provider.dart';
 
-class CounterScreen extends StatelessWidget {
+class CounterScreen extends ConsumerWidget {
   const CounterScreen({super.key});
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
+    final int clickCounter = ref.watch(counterProvider);
+    final isDarkMode = ref.watch(isDarkModeProvider);
+
     return Scaffold(
       appBar: AppBar(
         title: const Text('Counter Screen'),
+        actions: [
+          IconButton(
+            icon: Icon(isDarkMode ? Icons.dark_mode_outlined : Icons.light_mode_outlined),
+            onPressed: () {
+              ref.read(isDarkModeProvider.notifier).update((isDarkMode) => !isDarkMode);
+            }
+          )
+        ],
       ),
       body: Center(
-        child: Text('Value: 10', style: Theme.of(context).textTheme.titleLarge)
+        child: Text('Value: $clickCounter', style: Theme.of(context).textTheme.titleLarge)
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: () {},
+        onPressed: () {
+          ref.read(counterProvider.notifier).update((state) => state + 1);
+        },
         child: const Icon(Icons.add)
       ),
     );
